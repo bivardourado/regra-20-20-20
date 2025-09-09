2828c15b3cb4c1ba8ac85410246f714d1c7fc466
Bivar Dourado <95388204+bivardourado@users.noreply.github.com>
2025-09-09 17:40:53 -0300

    Update vite.config.js
    atualiza

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/regra-20-20-20/', // Adicionado para GitHub Pages
 })
